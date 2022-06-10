be1dc92012a8fdc3f16ed3bc5910412f15dac102
ShokseDev <aleshchenko.daniil@yandex.com>
2022-06-10 03:21:50 +0300

    finish layout

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    //Phone mask 
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+});
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__menu'),
     menuId = document.getElementById('menu'),
@@ -8,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('header__menu_active');
-        menuId.style.height = toggle ? '200px' : '0px'
+        menuId.style.height = toggle ? '200px' : '0px';
         toggle = !toggle;
     });
 
@@ -18,3 +23,18 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+}
